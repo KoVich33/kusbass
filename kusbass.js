8d3bf6f06c8681b9a28a1b84262bd5bf574d8a69
KoVich <131267477+KoVich33@users.noreply.github.com>
2025-02-13 17:45:11 +0300

    Add files via upload

--- kusbass.js
+++ kusbass.js
@@ -1,18 +1,24 @@
 let count = 0;
 const counterElement = document.getElementById('counter');
-const kusbass = document.getElementById('image');
-document.addEventListener('contextmenu', event => event.preventDefault());
+const kusbass = document.getElementById('kusbass');
 
+count = Number(getCookie("score"))
+
+// block contextmenu
+document.addEventListener('contextmenu', event => event.preventDefault());
+counterElement.textContent = ` ${count}`;
 //sound
 const snd_click = new Audio('res/sound/kusbass_res_0001.ogg')
 
 kusbass.addEventListener('click', function(event) {
     count += 1;
     counterElement.textContent = ` ${count}`;
-    kusbass.style.width = '360px'
+    // setCookie("score","kusbass", 365)
+    kusbass.style.animation = "click 0.3s";
     snd_click.play()
     // kusbass.src = 'https://kovich33.github.io/HTML5game_test/kusbass/res/M0003.png'
-    setTimeout(setspriteback, 50)
+    setCookie("score", count, 365)
+    setTimeout(setspriteback, 80)
 
     if (count >= 3) {
         kusbass.src = 'res/img/kusbass02.png'
@@ -27,5 +33,27 @@ kusbass.addEventListener('click', function(event) {
 
 function setspriteback() {
     kusbass.style.width = '512px'
+    kusbass.style.animation = "";
 }
 
+function setCookie(name, value, daysToLive){
+    const date = new Date();
+    date.setTime(date.getTime() +  (daysToLive * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + date.toUTCString();
+    document.cookie = `${name}=${value}; ${expires}; path=/`
+}
+function deleteCookie(name){
+    setCookie(name, null, null);
+}
+function getCookie(name){
+    const cDecoded = decodeURIComponent(document.cookie);
+    const cArray = cDecoded.split("; ");
+    let result = null;
+    
+    cArray.forEach(element => {
+        if(element.indexOf(name) == 0){
+            result = element.substring(name.length + 1)
+        }
+    })
+    return result;
+}
